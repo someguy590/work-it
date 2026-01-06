fdc0bdea67ae335744d606185a7d8709e8ad5189
Jonathan Moyett <someguy590@gmail.com>
2026-01-06 12:47:01 -0600

    add create notification helper

--- app/src/main/java/com/someguy590/workit/MainActivity.kt
+++ app/src/main/java/com/someguy590/workit/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.someguy590.workit
 
+import android.app.Notification
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,8 +13,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.NotificationCompat
 import com.someguy590.workit.ui.theme.WorkItTheme
+import com.someguy590.workit.utils.CHANNEL_ID
 import com.someguy590.workit.utils.createNotificationChannel
+import java.util.UUID
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,6 +36,15 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
+
+private fun Context.createNotification(): Notification {
+    return NotificationCompat.Builder(this, CHANNEL_ID)
+        .setSmallIcon(R.drawable.outline_adb_24)
+        .setContentTitle("Title ${UUID.randomUUID()}")
+        .setContentText("Content ${UUID.randomUUID()}")
+        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+        .build()
 }
 
 @Composable
