91b765101f854093ad2dd1d936ebb93c8f1b6732
Jonathan Moyett <someguy590@gmail.com>
2026-01-06 13:47:00 -0600

    make creation of channel backwards compatible

--- app/src/main/java/com/someguy590/workit/utils/Notifications.kt
+++ app/src/main/java/com/someguy590/workit/utils/Notifications.kt
@@ -1,10 +1,8 @@
 package com.someguy590.workit.utils
 
-import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
-import android.content.Context.NOTIFICATION_SERVICE
-import android.os.Build
+import androidx.core.app.NotificationChannelCompat
 import androidx.core.app.NotificationManagerCompat
 
 const val CHANNEL_ID = "0"
@@ -12,8 +10,10 @@ fun Context.createNotificationChannel() {
     val name = "WORK_IT"
     val description = "General Work It notifications"
     val importance = NotificationManager.IMPORTANCE_DEFAULT
-    val channel = NotificationChannel(CHANNEL_ID, name, importance)
-    channel.description = description
+    val channel = NotificationChannelCompat.Builder(CHANNEL_ID, importance)
+        .setName(name)
+        .setDescription(description)
+        .build()
     // Register the channel with the system. You can't change the importance
     // or other notification behaviors after this.
     NotificationManagerCompat.from(this).createNotificationChannel(channel)
