8c65705382671d53a6d51e76ebea88f16756c4fc
Jonathan Moyett <someguy590@gmail.com>
2026-01-07 15:25:53 -0600

    tidy

--- app/src/main/java/com/someguy590/workit/MainActivity.kt
+++ app/src/main/java/com/someguy590/workit/MainActivity.kt
@@ -4,15 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.someguy590.workit.ui.theme.WorkItTheme
 import com.someguy590.workit.ui.workout.WorkoutScreen
 import com.someguy590.workit.utils.createNotificationChannel
@@ -30,30 +25,5 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(
-    name: String,
-    handleSendNotification: () -> Unit,
-    modifier: Modifier = Modifier
-) {
-    Column {
-        Text(
-            text = "Hello $name!",
-            modifier = modifier
-        )
-        Button(handleSendNotification) {
-            Text("Send notification")
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    WorkItTheme {
-        Greeting("Android", {})
     }
 }
