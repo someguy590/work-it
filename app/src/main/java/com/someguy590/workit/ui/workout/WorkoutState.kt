1bad2fb0abc2cc765ac410bd9bbcf43f4abb6ab9
Jonathan Moyett <someguy590@gmail.com>
2026-01-09 16:06:13 -0600

    add edit mode to state

--- app/src/main/java/com/someguy590/workit/ui/workout/WorkoutState.kt
+++ app/src/main/java/com/someguy590/workit/ui/workout/WorkoutState.kt
@@ -3,5 +3,6 @@ package com.someguy590.workit.ui.workout
 import com.someguy590.workit.Workout
 
 data class WorkoutState(
-    val workouts: List<Workout> = emptyList()
+    val workouts: List<Workout> = emptyList(),
+    val isInEditMode: Boolean = false
 )
